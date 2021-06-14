9d3c8e966abc708ef2d63482851fc16319c2ba3d
devHybrid0306 <eddan0306@gmail.com>
2021-06-13 22:23:03 -0400

    [add] header, hamburger, main landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./app/containers/HomePage";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex-col
+  `}
+`;
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
+  );
 }
 
 export default App;
